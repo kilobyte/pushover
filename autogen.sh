9c094e4018c26d585601bdad589eb3569a6fb582
Adam Borowski <kilobyte@angband.pl>
2017-04-19 02:23:17 +0200

    Make autogen.sh work with git.
    darcs support is retained.

--- autogen.sh
+++ autogen.sh
@@ -2,8 +2,12 @@
 
 set -e
 
-export DARCS_DONT_ESCAPE_ISPRINT=1
-darcs changes > ChangeLog
+if [ -d _darcs ]; then
+    export DARCS_DONT_ESCAPE_ISPRINT=1
+    darcs changes > ChangeLog
+elif [ -d .git ]; then
+    git log --pretty=tformat:'%aD  %aN <%aE>%n  * %s%n%w(80,4,4)%+b' > ChangeLog
+fi
 
 echo po/*.po | sed 's,po/\([^ ]*\)\.po,\1,g' >po/LINGUAS
 sed -n '/^\(Hint\)$/,/^[^|]/ s,^| \(.*\)$,_("\1"),p' `find -name '*.level' | grep -v "levels/tests"` >po/leveltexts.txt
